eb9c5dd04e98c299315d05831665399af2902762
Huyptg1809 <huy.ptg.media@gmail.com>
2026-08-08 22:17:21 +0700

    Auto upload by HuyTool

---
+++ library_management/models/book_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class BookModel(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    price = Column(Float, nullable=False)
+    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
+    
+    author = relationship("AuthorModel", back_populates="books")
